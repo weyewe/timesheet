35b924451807db761e9f7f9a6f50be7ca0bf46f4
weyewe <w.yunnal@gmail.com>
2013-08-24 02:53:40 +0700

    cancel precompile all

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Timesheet::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += ['*.js', '*.css']
+  config.assets.precompile += ['application.js', 'application.css', 'bootstrap-email.css', 'bootstrap.css']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
